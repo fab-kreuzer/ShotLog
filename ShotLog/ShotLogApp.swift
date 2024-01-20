5c09100f292d3dba9c5ff3fa8e9626c97b231979
Fabian Kreuzer <kreuzer.fabian4@gmail.com>
2024-01-20 13:18:01 +0100

    Initial Commit

---
+++ ShotLog/ShotLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotLogApp.swift
+//  ShotLog
+//
+//  Created by Fabian Kreuzer on 20.01.24.
+//
+
+import SwiftUI
+
+@main
+struct ShotLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
